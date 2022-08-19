9f845f4fa776ba4c66eeb79d55987c27df8e8be1
Hua Zhou <huazhou@ucla.edu>
2022-08-19 15:01:42 -0700

    pgf for a center seems to be working

--- src/ClinicalTrialOptm.jl
+++ src/ClinicalTrialOptm.jl
@@ -1,5 +1,48 @@
 module ClinicalTrialOptm
 
-# Write your package code here.
+using Distributions, QuadGK
+export Country, pgf
+
+struct Country{T <: Real}
+m  :: T # mean of Gamma-distributed enrollment rate 
+s² :: T # var of Gamma-distributed enrollment rate 
+l  :: Integer # lower bound of number of centers
+u  :: Integer # upper bound of number of centers
+c₀ :: T # cost of initializing one center
+c  :: T # cost of running one center per unit of time
+q  :: T # cost per one enrolled patient
+d  :: T # probability of an enrolled patient dropping out
+T₀ :: Distribution{Univariate, Continuous} # distribution of center initialization time
+Td :: T # total duration of clinical trial
+end
+
+# constructor
+function Country(
+    m  :: T, # mean of Gamma-distributed enrollment rate 
+    s² :: T, # var of Gamma-distributed enrollment rate 
+    l  :: Integer, # lower bound of number of centers
+    u  :: Integer, # upper bound of number of centers
+    c₀ :: T, # cost of initializing one center
+    c  :: T, # cost of running one center per unit of time
+    q  :: T, # cost per one enrolled patient
+    d  :: T, # probability of an enrolled patient dropping out
+    T₀ :: Distribution{Univariate, Continuous}, # distribution of center initialization time    
+    Td :: T
+) where T <: Real
+    Country{T}(m, s², l, u, c₀, c, q, d, T₀, Td)
+end
+
+function pgf(
+    ctry :: Country{<:Real},
+    z :: Union{Real, Complex}
+    )
+    θ = ctry.s² / ctry.m
+    d = ctry.d
+    α = ctry.m^2 / ctry.s²
+    T₀ = ctry.T₀
+    Td = ctry.Td
+    (I, E) = quadgk(t -> (1 + θ * d * (Td - t) * (1 - z))^(-α) * pdf(T₀, t), minimum(ctry.T₀), maximum(ctry.T₀))
+    I
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using ClinicalTrialOptm
+using Distributions
 using Test
 
 @testset "ClinicalTrialOptm.jl" begin
-    # Write your tests here.
+    m = 1.5
+    s² = 2.0
+    l, u = 0, 10
+    c₀ = 20_000.0
+    c = 5_000.0
+    q = 1_500.0
+    d = 0.05
+    T₀ = Uniform(0.0, 6.0)
+    Td = 24.0
+    ctry = Country(m, s², l, u, c₀, c, q, d, T₀, Td)
+    @test ctry.m == m
+    @info "pgf of a center"
+    @test pgf(ctry, 1.0) == 1.0
 end
