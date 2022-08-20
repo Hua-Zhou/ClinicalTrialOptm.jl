3446933ac90276ea41adb2253f0192540cb4d6f1
Hua Zhou <huazhou@ucla.edu>
2022-08-19 21:59:00 -0700

    implement `mean` and `var` method for `Country`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using ClinicalTrialOptm
-using Distributions
+using Distributions, ForwardDiff
 using Test
 
 @testset "ClinicalTrialOptm.jl" begin
@@ -14,6 +14,16 @@ using Test
     Td = 24.0
     ctry = Country(m, s², l, u, c₀, c, q, d, T₀, Td)
     @test ctry.m == m
+    @info "mean/var of a center"
+    @show mean(ctry)
+    @show var(ctry)
     @info "pgf of a center"
     @test pgf(ctry, 1.0) == 1.0
+    # derivative of pgf at 1.0 == mean
+    @test ForwardDiff.derivative(z -> pgf(ctry, z), 1.0) ≈ mean(ctry)
+    # 2nd derivative of pgf at 1.0 == E[X(X-1)]
+    @test ForwardDiff.derivative(
+        t -> ForwardDiff.derivative(z -> pgf(ctry, z), t), 
+        1.0
+        ) ≈ var(ctry) + mean(ctry)^2 - mean(ctry)
 end
