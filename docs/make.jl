2a5b61412b5b18b0b4ef4fad1570aa9d157491d5
arinvansomphone <avansomphone@gmail.com>
2023-02-10 23:59:32 -0800

    put in make.jl and project.toml

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,20 @@
+using Distributions, Documenter, HiGHS, JuMP, MathOptInterface,
+Pajarito, Plots, SCIP, StatsPlots
 
+makedocs(
+    format = Documenter.HTML(),
+    sitename = "ClinicalTrialOptm.jl",
+    authors = "Arin Vansomphone, Hua Zhou",
+    clean = true,
+    debug = true,
+    pages = [
+        "index.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/Hua-Zhou/ClinicalTrialOptm.jl.git"
+    target = "build",
+    deps = nothing,
+    make = nothing
+)
