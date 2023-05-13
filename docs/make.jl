5e0596f0384f315c30a62c598f2fd28a19dcaf65
Arin Vansomphone <110351617+arinvansomphone@users.noreply.github.com>
2023-05-13 14:59:09 -0700

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,13 @@
-using Distributions, Documenter, HiGHS, JuMP, MathOptInterface,
-Pajarito, Plots, SCIP, StatsPlots
-
+# Inside make.jl
+push!(LOAD_PATH,"../src/")
+using ClinicalTrialOptm
+using Documenter
 makedocs(
-    format = Documenter.HTML(),
-    sitename = "ClinicalTrialOptm.jl",
-    authors = "Arin Vansomphone, Hua Zhou",
-    clean = true,
-    debug = true,
-    pages = [
-        "index.md"
-    ]
-)
-
-deploydocs(
-    repo = "github.com/Hua-Zhou/ClinicalTrialOptm.jl.git"
-    target = "build",
-    deps = nothing,
-    make = nothing
+         sitename = "ClinicalTrialOptm.jl",
+         modules  = [ClinicalTrialOptm],
+         pages=[
+                "Home" => "index.md"
+               ])
+deploydocs(;
+    repo="github.com/Hua-Zhou/ClinicalTrialOptm.jl",
 )
