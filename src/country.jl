05b55bddef739b20a05bad8bc9709392a2cf0f90
Joleen Vansomphone <joleensiri@gmail.com>
2026-07-04 17:47:16 -0700

    Update country.jl
    removed redundant outer constructor that caused precompile error on Julia v 1.12

--- src/country.jl
+++ src/country.jl
@@ -1,17 +1,3 @@
-struct Country{T <: Real}
-    m  :: T # mean of Gamma-distributed enrollment rate 
-    s² :: T # var of Gamma-distributed enrollment rate 
-    l  :: Integer # lower bound of number of centers
-    u  :: Integer # upper bound of number of centers
-    c₀ :: T # cost of initializing one center
-    c  :: T # cost of running one center per unit of time
-    q  :: T # cost per one enrolled patient
-    d  :: T # probability of an enrolled patient dropping out
-    T₀ :: Distribution{Univariate, Continuous} # distribution of center initialization time
-    Td :: T # total duration of clinical trial
-end
-
-#constructor
 """ 
     Country(
         m  :: T, 
@@ -44,19 +30,17 @@ See also [`mean`](@ref), [`var`](@ref),
 [`mean_cost`](@ref), [`pgf`](@ref),
 [`pmf`](@ref)
 """
-function Country(
-    m  :: T, # mean of Gamma-distributed enrollment rate 
-    s² :: T, # var of Gamma-distributed enrollment rate 
-    l  :: Integer, # lower bound of number of centers
-    u  :: Integer, # upper bound of number of centers
-    c₀ :: T, # cost of initializing one center
-    c  :: T, # cost of running one center per unit of time
-    q  :: T, # cost per one enrolled patient
-    d  :: T, # probability of an enrolled patient dropping out
-    T₀ :: Distribution{Univariate, Continuous}, # distribution of center initialization time    
-    Td :: T
-    ) where T <: Real
-    Country{T}(m, s², l, u, c₀, c, q, d, T₀, Td)
+struct Country{T <: Real}
+    m  :: T # mean of Gamma-distributed enrollment rate 
+    s² :: T # var of Gamma-distributed enrollment rate 
+    l  :: Integer # lower bound of number of centers
+    u  :: Integer # upper bound of number of centers
+    c₀ :: T # cost of initializing one center
+    c  :: T # cost of running one center per unit of time
+    q  :: T # cost per one enrolled patient
+    d  :: T # probability of an enrolled patient dropping out
+    T₀ :: Distribution{Univariate, Continuous} # distribution of center initialization time
+    Td :: T # total duration of clinical trial
 end
 
 """ 
